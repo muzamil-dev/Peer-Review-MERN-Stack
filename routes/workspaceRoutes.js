5e2721ec1971221a361e07003038bedafa6df80a
ckrick04 <ckr20004@gmail.com>
2024-06-12 08:59:02 -0400

    Added endpoint for setting invite code

--- routes/workspaceRoutes.js
+++ routes/workspaceRoutes.js
@@ -4,6 +4,7 @@ import { Workspace } from '../models/workspaceModel.js';
 
 import { checkWorkspace, checkUser } from "../shared/checks.js";
 import { addUserToWorkspace, addWorkspaceToUser } from "../shared/adders.js";
+import generateInviteCode from '../shared/inviteCode.js';
 
 const router = express.Router();
 
@@ -45,8 +46,7 @@ router.post("/", async(req, res) => {
 
 router.put("/join", async(req, res) => {
     try{
-        // Get workspace id
-        // Check for the user id
+        // Check for the workspace id and user id
         const body = req.body;
         if (!body.workspaceId || !body.userId){
             return res.status(400).json({ message: "One or more required fields is not present" });
@@ -77,6 +77,52 @@ router.put("/join", async(req, res) => {
             ]
         );
         res.status(200).send({ message: "Workspace joined successfully!"});
+    }
+    catch(err){
+        console.log(err.message);
+        res.status(500).send({ message: err.message });
+    }
+});
+
+router.put("/setInvite", async(req, res) => {
+    // Check for the workspace id and user id
+    try{
+        const body = req.body;
+        if (!body.workspaceId || !body.userId){
+            return res.status(400).json({ message: "One or more required fields is not present" });
+        }
+        // Check if workspace and user exist
+        const [workspaceId, userId] = await Promise.all(
+            [
+                checkWorkspace(body.workspaceId), 
+                checkUser(body.userId)
+            ]
+        );
+        if (!workspaceId){
+            return res.status(400).json({ message: "The provided workspace was not found in our database" });
+        }
+        if (!userId){
+            return res.status(400).json({ message: "The provided user was not found in our database" });
+        }
+        // Check that the user is authorized to send the invite
+        const workspaceMembers = (await Workspace.findById(workspaceId)).userIds;
+        const found = workspaceMembers.find(user => user.userId.equals(userId));
+        if (!found){
+            return res.status(400).json({ message: "The provided user is not a member of this workspace" });
+        }
+        else if (found.role !== "Instructor"){
+            return res.status(403).json({ message: "The provided user is not authorized to make this request" });
+        }
+        // Set the invite code
+        const inviteCode = body.inviteCode ? body.inviteCode : generateInviteCode();
+        const update = { inviteCode };
+        // Throws error if invalid date is given, sets to null if none is provided
+        update.inviteCodeExpiry = body.inviteCodeExpiry ? new Date(body.inviteCodeExpiry) : null;
+        await Workspace.updateOne(
+            { _id: workspaceId },
+            update
+        );
+        return res.json({ message: "Invite code updated successfully" });
     }
     catch(err){
         console.log(err.message);
