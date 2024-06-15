e7317e7a0888a9ab66ee400824ba9518980d176b
HashDBrown <halkhateeb704@gmail.com>
2024-06-15 16:57:01 -0400

    yes

---
+++ backend/middleware/checks.js
@@ -0,0 +1,132 @@
+import { Group } from "../models/groupModel.js";
+import { ReviewAssignment } from "../models/reviewAssignmentModel.js";
+import { User } from "../models/userModel.js";
+import { Workspace } from "../models/workspaceModel.js";
+
+// Check if the workspace is in the body
+export async function checkWorkspace(req, res, next){
+    if (req.params?.workspaceId){
+        if (!req.body)
+            req.body = {};
+        req.body.workspaceId = req.params.workspaceId;
+    }
+    const workspace = await Workspace.findById(
+        req.body.workspaceId
+    );
+    if (!workspace)
+        return res.status(400).json({ message: "The provided workspace was not found in our database" });
+    next();
+}
+
+// Check if the user exists
+export async function checkUser(req, res, next){
+    const user = await User.findById(
+        req.body.userId
+    );
+    if (!user)
+        return res.status(400).json({ message: "The provided user was not found in our database" });
+    next();
+}
+
+// Check if the group exists
+export async function checkGroup(req, res, next){
+    if (req.params?.groupId){
+        if (!req.body)
+            req.body = {};
+        req.body.groupId = req.params.groupId;
+    }
+    const group = await Group.findById(
+        req.body.groupId
+    );
+    if (!group)
+        return res.status(400).json({ message: "The provided group was not found in our database" });
+    // For use with other middleware
+    req.body.workspaceId = group.workspaceId;
+    next();
+}
+
+// Check that the assignmentId is provided (for creating a review)
+export async function checkAssignment(req, res, next){
+    if (req.params?.assignmentId){
+        if (!req.body)
+            req.body = {};
+        req.body.assignmentId = req.params.assignmentId;
+    }
+    const assignment = await ReviewAssignment.findById(
+        req.body.assignmentId
+    );
+    if (!assignment){
+        return res.status(400).json({
+            message: "The provided assignment was not found in our database" 
+        });
+    }
+    next();
+} 
+
+
+// Checks if the user is in the group (assumes workspace is provided)
+// TODO: Change the function to check that the user is not in any group
+export async function checkUserNotInGroup(req, res, next){
+    // Get all groups from workspace
+    const allGroups = await Group.find(
+        { workspaceId: req.body.workspaceId }
+    ).select('userIds');
+    // Get all users into one array
+    const allUsers = [];
+    for (let group of allGroups){
+        allUsers.push(...group.userIds);
+    }
+    // Find the user in all of the users in groups
+    const found = allUsers.find(
+        user => user.equals(req.body.userId)
+    );
+    if (found){
+        return res.status(400).json({ message: "User is already a member of a group in this workspace" });
+    }
+    next();
+}
+
+// Checks if the user is in the workspace. Assumes existence of workspace is checked
+export async function checkUserInWorkspace(req, res, next){
+    // Search for target in workspace
+    const workspaceUsers = (await Workspace.findById(
+        req.body.workspaceId
+    ).select('userIds')).userIds;
+    const found = workspaceUsers.find(
+        user => user.userId.equals(req.body.userId)
+    );
+    if (!found){
+        return res.status(400).json({ message: "The provided user was not found in this workspace" });
+    }
+    next();
+}
+
+// Checks if the target user is in the workspace. Assumes existence of workspace is checked
+export async function checkTargetInWorkspace(req, res, next){
+    // Search for target in workspace
+    const workspaceUsers = (await Workspace.findById(
+        req.body.workspaceId
+    ).select('userIds')).userIds;
+    const found = workspaceUsers.find(
+        user => user.userId.equals(req.body.targetId)
+    );
+    if (!found){
+        return res.status(400).json({ message: "The provided user was not found in this workspace" });
+    }
+    next();
+}
+
+// Checks if the given user is an instructor
+// User/workspace should be checked before using this function
+export async function checkInstructor(req, res, next){
+    const body = req.body;
+    const workspaceMembers = (await Workspace.findById(body.workspaceId)).userIds;
+    const found = workspaceMembers.find(user => user.userId.equals(body.userId));
+    if (!found){
+        return res.status(400).json({ message: "The provided user is not a member of this workspace" });
+    }
+    else if (found.role !== "Instructor"){
+        return res.status(403).json({ message: "The provided user is not authorized to make this request" });
+    }
+    next();
+}
---
+++ backend/routes/reviewAssignmentRoutes.js
@@ -0,0 +1,141 @@
+import express from "express";
+import { Review } from "../models/reviewsModel.js";
+import { User } from "../models/userModel.js";
+import { Group } from "../models/groupModel.js";
+import { Workspace } from "../models/workspaceModel.js"
+import { ReviewAssignment } from "../models/reviewAssignmentModel.js";
+
+import { checkWorkspace, checkInstructor } from "../middleware/checks.js";
+
+const router = express.Router();
+
+// Get the reviews that a user has done on a specific assignment
+router.get("/:assignmentId/reviews/:userId", async(req, res) => {
+    try{
+        const { assignmentId, userId } = req.params;
+        // Get info about assignment
+        const assignment = await ReviewAssignment.findById(
+            assignmentId
+        ).select('workspaceId questions');
+        // Get groups from workspace that contain userId
+        const group = await Group.findOne({
+            workspaceId: assignment.workspaceId,
+            userIds: userId
+        });
+
+        // Keep track of complete vs incomplete reviews
+        const completed = [];
+        const notCompleted = [];
+        
+        // Remove the id of the user being searched
+        group.userIds = group.userIds.filter(
+            id => !id.equals(userId)
+        );
+        group.userIds = group.userIds.map(id => id.toString());
+        // Gets all reviews by user for group members
+        const groupReviews = await Review.find({
+            assignmentId,
+            userId,
+            targetId: { $in: group.userIds }
+        }).select('userId targetId');
+
+        // Get targetIds for completed reviews and incomplete reviews
+        const completedIds = groupReviews.map(
+            review => review.targetId
+        );
+        const reviewIds = groupReviews.map(
+            review => review._id
+        );
+
+        // Filter out completedIds from all userIds
+        const notCompletedIds = group.userIds.filter(
+            id1 => !completedIds.some(id2 => id1.equals(id2))
+        );
+
+        // Get names of group members
+        let [completeUsers, incompleteUsers] = await Promise.all([
+            User.find({ _id: { $in: completedIds }}).select('firstName middleName lastName'),
+            User.find({ _id: { $in: notCompletedIds }}).select('firstName middleName lastName')
+        ]);
+
+        // Change name of fields
+        completeUsers = completeUsers.map(
+            (user, index) => ({ targetId: user._id, reviewId: reviewIds[index], targetFirstName: user.firstName, 
+            targetMiddleName: user.middleName, targetLastName: user.lastName })
+        );
+        incompleteUsers = incompleteUsers.map(
+            user => ({ targetId: user._id, targetFirstName: user.firstName, 
+            targetMiddleName: user.middleName, targetLastName: user.lastName })
+        );
+
+        // Add the reviews to the arrays
+        completed.push(...completeUsers);
+        notCompleted.push(...incompleteUsers);
+        
+        res.json({ assignmentId, userId, completed, notCompleted });
+    }
+    catch(err){
+        console.log(err.message);
+        return res.status(500).send({ message: err.message });
+    }
+});
+
+// Excluding assignedReviews for now, will likely be unnecessary
+router.post("/create", checkWorkspace, checkInstructor, async(req, res) => {
+    try{
+        const body = req.body;
+        const workspaceId = body.workspaceId
+        // Check for required fields
+        if (!body.startDate || !body.dueDate || !body.questions || body.questions.length < 1){
+            return res.status(400).json({ message: "One or more required fields is not present" });
+        }
+        // Create assigned reviews array (to populate)
+        const assignedReviews = [];
+        // Get all groups in the workspace
+        const groups = await Group.find(
+            { workspaceId }
+        ).select('userIds');
+
+        /*
+        // Populate assignedReviews
+        for (let group of groups){
+            const userIds = group.userIds;
+            const groupId = group._id;
+            for (let j = 0; j < userIds.length; j++){
+                const userReviews = { userId: userIds[j], reviews: [] };
+                for (let k = 0; k < userIds.length; k++){
+                    if (j !== k){
+                        userReviews.reviews.push({
+                            targetId: userIds[k],
+                            reviewId: null
+                        });
+                    }
+                }
+                assignedReviews.push(userReviews);
+            }
+        }
+        */
+
+        // Get other fields
+        const startDate = new Date(body.startDate);
+        const dueDate = new Date(body.dueDate);
+        const questions = body.questions;
+        const description = body.description;
+
+        // Get the returned object
+        const assignment = (await ReviewAssignment.create(
+            { workspaceId, description, startDate, dueDate, questions} //, assignedReviews }
+        )).toObject();
+
+        // assignedReviews takes up a lot of space, so its not included
+        // delete assignment.assignedReviews;
+
+        return res.json(assignment);
+    }
+    catch(err){
+        console.log(err.message);
+        return res.status(500).send({ message: err.message });
+    }
+});
+
+export default router;
