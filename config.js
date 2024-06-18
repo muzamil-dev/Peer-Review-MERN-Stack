62e377e9e82f9ef946a9d8d3a451d92f14c96edf
HashDBrown <halkhateeb704@gmail.com>
2024-06-18 16:38:17 -0400

    test

--- config.js
+++
@@ -1,9 +0,0 @@
-export const PORT = 5000;
-export const mongoDBURL =
-    "mongodb+srv://ckrick04:zTqXKXg6eM2bNfej@test.ym8e81f.mongodb.net/hash?retryWrites=true&w=majority&appName=TEST"
-
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const emailUser = process.env.EMAIL_USER;
-export const emailPass = process.env.EMAIL_PASS;
