1464d5f4b85a90ea01c70998b302ab553199fbfe
HashDBrown <halkhateeb704@gmail.com>
2024-06-18 03:46:26 -0400

    pullef my changes from front

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application/src/app.dart';
 import 'package:flutter_application/src/dashboard/admin_dashboard.dart';
 import 'package:flutter_application/src/dashboard/user_dashboard.dart';
-
 import 'src/login-signup/loginsignup.dart';
 import 'src/login-signup/signup.dart';
 import 'src/login-signup/login.dart';
--- flutter_application/lib/src/login-signup/resetPassword.dart
+++ flutter_application/lib/src/login-signup/resetPassword.dart
@@ -1,4 +1,121 @@
-//page to reset password
 import 'package:flutter/material.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_application/src/login-signup/loginsignup.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ResetPasswordScreen extends StatefulWidget {
+  static const routeName = '/resetPassword'; // Add route name
+
+  final String token;
+
+  const ResetPasswordScreen({Key? key, required this.token}) : super(key: key);
+
+  @override
+  _ResetPasswordScreenState createState() => _ResetPasswordScreenState();
+}
+
+class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
+  final TextEditingController newPasswordController = TextEditingController();
+  final TextEditingController confirmNewPasswordController = TextEditingController();
+
+  Future<void> resetPassword(String token, String newPassword) async {
+    final url = Uri.parse('http://10.0.2.2:5000/users/resetPassword');
+
+    try{
+      final response = await http.post(
+        url,
+        headers: <String, String>{
+          'Content-Type': 'application/json;',
+        },
+        body: jsonEncode({
+          'token': token,
+          'newPassword': newPassword,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('Password reset successful');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Password reset successfully. Please login with your new password.')),
+        );
+        Navigator.pushNamed(context, '/login');
+      } else {
+        final errorData = jsonDecode(response.body);
+        print('Password reset failed: ${errorData['message']}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Password reset failed: ${errorData['message']}')),
+        );
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reset Password'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: newPasswordController,
+              decoration: InputDecoration(
+                labelText: 'New Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide(color: Colors.blue),
+                ),
+                fillColor: Colors.purple.withOpacity(0.1),
+                filled: true,
+                prefixIcon: const Icon(Icons.lock),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: confirmNewPasswordController,
+              decoration: InputDecoration(
+                labelText: 'Confirm New Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide(color: Colors.blue),
+                ),
+                fillColor: Colors.purple.withOpacity(0.1),
+                filled: true,
+                prefixIcon: const Icon(Icons.lock),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                final newPassword = newPasswordController.text;
+                final confirmNewPassword = confirmNewPasswordController.text;
+
+                if (newPassword.isEmpty || confirmNewPassword.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Please fill in all fields')),
+                  );
+                  return;
+                }
+
+                if (newPassword == confirmNewPassword) {
+                  resetPassword(widget.token, newPassword);
+                }else{
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Passwords do not match')),
+                  );
+                }
+              },
+              child: const Text('Reset Password'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
