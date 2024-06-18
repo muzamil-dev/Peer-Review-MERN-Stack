24af1b3202cb1f8f71d7afb6dcba13006a15be92
ckrick04 <ckr20004@gmail.com>
2024-06-17 20:41:01 -0400

    Heavily simplified get route on assignments

--- routes/reviewAssignmentRoutes.js
+++ routes/reviewAssignmentRoutes.js
@@ -6,84 +6,27 @@ import { Workspace } from "../models/workspaceModel.js"
 import { ReviewAssignment } from "../models/reviewAssignmentModel.js";
 
 import * as Checkers from "../shared/checkers.js";
+import { getUserReviewsByAssignment } from "../shared/getters.js";
 
 const router = express.Router();
 
-// Get the reviews that a user has done on a specific assignment
-// TODO: Simplify using the complete/imcomplete boolean
+// Get all reviews for a given assignment/user
 router.get(["/:assignmentId/user", "/:assignmentId/user/:userId"], async(req, res) => {
     try{
         // Get assignmentId and userId from params
         const { assignmentId } = req.params;
         const userId = req.params.userId || req.body.userId;
 
-        // Get info about assignment
-        const assignment = await ReviewAssignment.findById(
-            assignmentId
-        ).select('workspaceId questions');
-
-        // If a userId param is passed, check that an instructor is making the request
-        if (req.params.userId && 
-            !await Checkers.checkInstructor(req.body.userId, assignment.workspaceId)){
-            return res.status(403).json({ message: "User is not authorized to make this request" });
-        }
-
-        // Get groups from workspace that contain userId
-        const group = await Group.findOne({
-            workspaceId: assignment.workspaceId,
-            userIds: userId
-        });
-
-        // Keep track of complete vs incomplete reviews
-        const completed = [];
-        const notCompleted = [];
+        // Check that the assignment exists
+        const assignment = await ReviewAssignment.findById(assignmentId);
+        if (!assignment)
+            return res.status(404).json({ 
+                message: "The provided assignment wasn't found in our database" 
+            });
         
-        // Remove the id of the user being searched
-        group.userIds = group.userIds.filter(
-            id => !id.equals(userId)
-        );
-        group.userIds = group.userIds.map(id => id.toString());
-        // Gets all reviews by user for group members
-        const groupReviews = await Review.find({
-            assignmentId,
-            userId,
-            targetId: { $in: group.userIds }
-        }).select('userId targetId');
-
-        // Get targetIds for completed reviews and incomplete reviews
-        const completedIds = groupReviews.map(
-            review => review.targetId
-        );
-        const reviewIds = groupReviews.map(
-            review => review._id
-        );
-
-        // Filter out completedIds from all userIds
-        const notCompletedIds = group.userIds.filter(
-            id1 => !completedIds.some(id2 => id1.equals(id2))
-        );
-
-        // Get names of group members
-        let [completeUsers, incompleteUsers] = await Promise.all([
-            User.find({ _id: { $in: completedIds }}).select('firstName middleName lastName'),
-            User.find({ _id: { $in: notCompletedIds }}).select('firstName middleName lastName')
-        ]);
-
-        // Change name of fields
-        completeUsers = completeUsers.map(
-            (user, index) => ({ targetId: user._id, reviewId: reviewIds[index], targetFirstName: user.firstName, 
-            targetMiddleName: user.middleName, targetLastName: user.lastName })
-        );
-        incompleteUsers = incompleteUsers.map(
-            user => ({ targetId: user._id, targetFirstName: user.firstName, 
-            targetMiddleName: user.middleName, targetLastName: user.lastName })
-        );
-
-        // Add the reviews to the arrays
-        completed.push(...completeUsers);
-        notCompleted.push(...incompleteUsers);
-        
-        res.json({ assignmentId, userId, completed, notCompleted });
+        // Get the reviews
+        const reviews = await getUserReviewsByAssignment(userId, assignmentId);
+        res.json(reviews);
     }
     catch(err){
         console.log(err.message);
--- shared/getters.js
+++ shared/getters.js
@@ -1,6 +1,7 @@
 import { Group } from "../models/groupModel.js";
 import { User } from "../models/userModel.js";
 import { Workspace } from "../models/workspaceModel.js";
+import { Review } from "../models/reviewsModel.js";
 
 export async function getGroupInWorkspace(userId, workspaceId){
     const user = await User.findById(
@@ -33,4 +34,43 @@ export async function getGroupData(groupId){
         groupId: group._id,
         members
     });
+}
+
+// Takes in assignmentId and userId, assumes both exist
+export async function getUserReviewsByAssignment(userId, assignmentId){
+    // Get reviews
+    const reviews = await Review.find(
+        { assignmentId, userId }
+    ).select('targetId ratings completed').sort({ targetId: 1 });
+    // Get the name of the user
+    const user = await User.findById(userId).select('firstName middleName lastName');
+    // Get ids for all members
+    const targets = reviews.map(review => review.targetId);
+    // Get names for all targets
+    const targetNames = await User.find(
+        { _id: { $in: targets }}
+    ).select('firstName middleName lastName').sort({ _id: 1 });
+    // Match the targetNames to the reviews (formatted)
+    const formatted = reviews.map(
+        (review, index) => ({
+            targetId: review.targetId,
+            firstName: targetNames[index].firstName,
+            middleName: targetNames[index].middleName,
+            lastName: targetNames[index].lastName,
+            ratings: review.ratings,
+            completed: review.completed
+        })
+    );
+    // Filter the reviews
+    const completedReviews = formatted.filter(review => review.completed);
+    const incompleteReviews = formatted.filter(review => !review.completed);
+    // Return the formatted object
+    return {
+        userId,
+        firstName: user.firstName,
+        middleName: user.middleName,
+        lastName: user.lastName,
+        completedReviews,
+        incompleteReviews
+    };
 }
