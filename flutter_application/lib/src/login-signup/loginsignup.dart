a43aea06c2f767d61e14a6e7ad3918a839f646ad
HashDBrown <halkhateeb704@gmail.com>
2024-06-20 01:02:07 -0400

    some scroll thing

--- flutter_application/lib/src/login-signup/loginsignup.dart
+++ flutter_application/lib/src/login-signup/loginsignup.dart
@@ -265,49 +265,51 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(20),
-                margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 160),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text(
-                      'Reset Password',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      controller : resetEmailController,
-                      decoration: InputDecoration(
-                        labelText: 'Enter your email',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(18),
-                          borderSide: BorderSide.none,
+              SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 160),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        'Reset Password',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
-                        fillColor: Colors.grey[200],
-                        filled: true,
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle password reset logic
-                        // Send password reset email
-                        final email = resetEmailController.text;
-                        requestPasswordReset(context, email);
-                      },
-                      child: const Text('Submit'),
-                    ),
-                  ],
+                      const SizedBox(height: 20),
+                      TextField(
+                        controller : resetEmailController,
+                        decoration: InputDecoration(
+                          labelText: 'Enter your email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18),
+                            borderSide: BorderSide.none,
+                          ),
+                          fillColor: Colors.grey[200],
+                          filled: true,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Handle password reset logic
+                          // Send password reset email
+                          final email = resetEmailController.text;
+                          requestPasswordReset(context, email);
+                        },
+                        child: const Text('Submit'),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              )
             ],
           ),
           ),
