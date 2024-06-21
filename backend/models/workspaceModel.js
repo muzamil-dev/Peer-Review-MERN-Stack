86ef1049b3a7bcf1b004451e4a17f8aff9a979c1
HashDBrown <halkhateeb704@gmail.com>
2024-06-21 02:16:36 -0400

    got domain working

--- backend/models/workspaceModel.js
+++ backend/models/workspaceModel.js
@@ -27,6 +27,11 @@ const workspaceSchema = mongoose.Schema(
             default: [],
             required: true,
         },
+        groupMemberLimit: {
+            type: Number,
+            default: null,
+            required: false
+        },
         allowedDomains: {
             type: [String],
             default: [],
