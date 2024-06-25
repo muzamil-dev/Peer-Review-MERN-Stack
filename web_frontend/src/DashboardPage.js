79770b008e2714ad95577ba51655b5ca065d62e2
HashDBrown <halkhateeb704@gmail.com>
2024-06-25 01:32:37 -0400

    redesigned home page

--- web_frontend/src/DashboardPage.js
+++ web_frontend/src/DashboardPage.js
@@ -5,11 +5,11 @@ import './DashboardPage.css';
 const DashboardPage = () => {
     const [workspaces, setWorkspaces] = useState([
         { id: 1, name: 'Workspace 1', role: 'Admin' },
-        // { id: 2, name: 'Workspace 2', role: 'User' },
-        // { id: 3, name: 'Workspace 3', role: 'User' },
-        // { id: 4, name: 'Workspace 4', role: 'User' },
-        // { id: 5, name: 'Workspace 5', role: 'User' },
-        // { id: 6, name: 'Workspace 6', role: 'User' },
+        { id: 2, name: 'Workspace 2', role: 'User' },
+        { id: 3, name: 'Workspace 3', role: 'User' },
+        { id: 4, name: 'Workspace 4', role: 'User' },
+        { id: 5, name: 'Workspace 5', role: 'User' },
+        { id: 6, name: 'Workspace 6', role: 'User' },
         // { id: 7, name: 'Workspace 7', role: 'User' },
         // { id: 8, name: 'Workspace 8', role: 'Admin' },
         // { id: 9, name: 'Workspace 9', role: 'User' },
@@ -86,8 +86,8 @@ const DashboardPage = () => {
     return (
         <div className="dashboard">
             <h1 className="header-large">Workspaces</h1>
-            <div className="main-container">
-                <div className="workspace-cards">
+            <div className="container">
+                <div className="row workspace-cards">
                     {workspaces.map((workspace) => (
                         <div key={workspace.id} className="workspace-card" onClick={() => handleWorkspaceClick(workspace.id)}>
                             <h2>{workspace.name}</h2>
@@ -95,56 +95,87 @@ const DashboardPage = () => {
                         </div>
                     ))}
                 </div>
-                <div className="workspace-actions">
-                    <h2>Join Workspace</h2>
-                    <input
-                        type="text"
-                        placeholder="Enter code"
-                        value={inviteCode}
-                        onChange={(e) => setInviteCode(e.target.value)}
-                        className="form-control mb-2 transparent-input"
-                    />
-                    <button onClick={handleJoinWorkspace} className="btn btn-primary mb-4">Join</button>
-                    <h1>OR</h1>
-                    <h2>Create Workspace</h2>
-                    <input
-                        type="text"
-                        placeholder="ex. COP 4331c"
-                        value={newWorkspaceName}
-                        onChange={(e) => setNewWorkspaceName(e.target.value)}
-                        className="form-control mb-2 transparent-input"
-                    />
-                    <h3>Domain Restrictions</h3>
-                    {newWorkspaceDomain.map((domain, index) => (
-                        <div key={index} className="domain-restriction mb-2">
+                <div className="workspace-actions text-center">
+                    <button type="button" className="btn btn-primary btn-large mb-4" data-toggle="modal" data-target="#joinWorkspaceModal">
+                        Join
+                    </button>
+                    <button type="button" className="btn btn-success btn-large mb-4" data-toggle="modal" data-target="#createWorkspaceModal">
+                        Create
+                    </button>
+                </div>
+            </div>
+            {/* Join Workspace Modal */}
+            <div className="modal fade" id="joinWorkspaceModal" tabIndex="-1" aria-labelledby="joinWorkspaceModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="joinWorkspaceModalLabel">Join Workspace</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
                             <input
                                 type="text"
-                                placeholder="ex. ucf.edu"
-                                value={domain}
-                                onChange={(e) => handleDomainChange(index, e.target.value)}
-                                className="form-control d-inline-block transparent-input"
-                                style={{ width: 'calc(100% - 0px)' }}
+                                placeholder="Enter code"
+                                value={inviteCode}
+                                onChange={(e) => setInviteCode(e.target.value)}
+                                className="form-control mb-2"
                             />
-                            <button onClick={() => handleDeleteDomain(index)} className="btn btn-danger ml-2">🗑️</button>
                         </div>
-                    ))}
-                    <button onClick={handleAddDomain} className="btn btn-secondary mb-4">➕</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary" onClick={handleJoinWorkspace}>Join</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-                    <input
-                        type="text"
-                        placeholder="Max Group Size"
-                        value={maxGroupSize}
-                        onChange={(e) => setMaxGroupSize(e.target.value)}
-                        className="form-control mb-2 transparent-input"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Number of Groups"
-                        value={numGroups}
-                        onChange={(e) => setNumGroups(e.target.value)}
-                        className="form-control mb-2 transparent-input"
-                    />
-                    <button onClick={handleAddWorkspace} className="btn btn-success">Create</button>
+            {/* Create Workspace Modal */}
+            <div className="modal fade" id="createWorkspaceModal" tabIndex="-1" aria-labelledby="createWorkspaceModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="createWorkspaceModalLabel">Create Workspace</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <input
+                                type="text"
+                                placeholder="Workspace Name"
+                                value={newWorkspaceName}
+                                onChange={(e) => setNewWorkspaceName(e.target.value)}
+                                className="form-control mb-2"
+                            />
+                            <input
+                                type="text"
+                                placeholder="Domain Restriction"
+                                value={newWorkspaceDomain}
+                                onChange={(e) => setNewWorkspaceDomain(e.target.value)}
+                                className="form-control mb-2"
+                            />
+                            <input
+                                type="text"
+                                placeholder="Max Group Size"
+                                value={maxGroupSize}
+                                onChange={(e) => setMaxGroupSize(e.target.value)}
+                                className="form-control mb-2"
+                            />
+                            <input
+                                type="text"
+                                placeholder="Number of Groups"
+                                value={numGroups}
+                                onChange={(e) => setNumGroups(e.target.value)}
+                                className="form-control mb-2"
+                            />
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-success" onClick={handleAddWorkspace}>Create</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
