2399fa7e2c57c4fd805bf78854618f4e88b48d3b
ckrick04 <ckr20004@gmail.com>
2024-06-27 14:57:17 -0400

    Moved config variables to .env, hide in prod

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,15 @@
 import pg from "pg"
+import dotenv from "dotenv";
 
+dotenv.config();
+
+// Fill in client information from .env
 const db = new pg.Client({
-    host: "localhost",
-    port: 5433,
-    user: "cdog",
-    password: "password",
-    database: "peer"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 export default db;
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import db from "./config.js";
 
+dotenv.config();
+
+// Set the port
+const PORT = process.env.PORT || 5000;
+
+// Initialize the app
 const app = express();
+app.use(express.json());
 
+// Connect to the database
 await db.connect();
+console.log(`Connected to the database.`); 
 
-const res = await db.query(`
-        insert into users (first_name, last_name, email, password)
-        values ($1, $2, $3, $4)
-    `, ['Rick1', 'Leinecker', 'rick1@ucf.edu', 'password']);
-
-await db.end();
-
-console.log(res);
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`);
+});
