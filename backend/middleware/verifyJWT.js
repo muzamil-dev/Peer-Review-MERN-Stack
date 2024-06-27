bbc453f227137ddd8b3bd2f31b5bc859c74cedda
HashDBrown <halkhateeb704@gmail.com>
2024-06-26 20:05:52 -0400

    fix

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
@@ -27,34 +26,4 @@ const verifyJWT = (req, res, next) => {
     );
 }
 
-=======
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if (!authHeader){
-        return res.status(401).json({ message: "JWT Verification failed - No token found" }); // Unauthorized
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET,
-        (err, payload) => {
-            if (err)
-                return res.status(403).json({ message: "JWT Verification failed - Incorrect token" });
-            if (!req.body)
-                req.body = {};
-
-            // Set user vars
-            req.body.userId = payload.userId;
-            // Go to next
-            next();
-        }
-    );
-}
-
->>>>>>> backend
 export default verifyJWT;
