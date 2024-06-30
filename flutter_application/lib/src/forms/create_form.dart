dcf80ecb92ef30a3e5a438336e2ccb70cca4ff43
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2024-06-30 11:39:26 -0400

    Added Base Layout for Add Forms Tab

--- flutter_application/lib/src/forms/create_form.dart
+++ flutter_application/lib/src/forms/create_form.dart
@@ -17,57 +17,209 @@ class CreateForm extends StatefulWidget {
 
 class _CreateFormState extends State<CreateForm> {
   int _currentIndex = 0;
-  
+  int numFields = 0;
+  TextEditingController availableFromController = TextEditingController();
+  TextEditingController dueUntillController = TextEditingController();
+  TextEditingController formName = TextEditingController();
 
-  List<Widget> _getTabOptions() {
-    return <Widget>[
-      editFormsPage(),
-      studentViewPage()
-    ];
+  List<Widget> _widgetTabOptions(BuildContext context) {
+    return <Widget>[editFormsPage(context), studentViewPage(context)];
   }
 
-  Widget editFormsPage() {
-    return const Column(
-      children: [
-        Center(child: Text("Edit Forms Page")),
-      ],
+  Future<void> _selectDate(TextEditingController controller) async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2024),
+      lastDate: DateTime(2100),
+    );
+
+    if (picked != null) {
+      setState(() {
+        controller.text = picked.toString().split(" ")[0];
+      });
+    }
+  }
+
+  Widget editFormsPage(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18),
+            child: Column(
+              children: [
+                Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Form Name
+                      const Text(
+                        "Form Name",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      const SizedBox(height: 5),
+                      TextFormField(
+                        controller: formName,
+                        decoration: const InputDecoration(
+                          labelText: 'Form Name',
+                          filled: true,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        controller: availableFromController,
+                        decoration: const InputDecoration(
+                          labelText: 'Available From',
+                          filled: true,
+                          prefixIcon: Icon(Icons.calendar_today),
+                        ),
+                        readOnly: true,
+                        onTap: () {
+                          _selectDate(availableFromController);
+                        },
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        controller: dueUntillController,
+                        decoration: const InputDecoration(
+                          labelText: 'Available Untill',
+                          filled: true,
+                          prefixIcon: Icon(Icons.calendar_month_outlined),
+                        ),
+                        readOnly: true,
+                        onTap: () {
+                          _selectDate(dueUntillController);
+                        },
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      // Form Settings
+
+                      // Form Fields (ListView Builder)
+                      const Text(
+                        'Fields',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      formFields(context),
+                      // Available from Untill
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            numFields += 1;
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 
-  Widget studentViewPage() {
-    return const Column(
-      children: [
-        Center(child: const Text("Student View Page"))
-      ],
+  Widget formFields(BuildContext context) {
+    return SizedBox(
+      height: 400,
+      child: Scrollbar(
+        child: ListView.separated(
+          itemBuilder: (context, index) {
+            return formChild(context, index);
+          },
+          separatorBuilder: (BuildContext context, index) => const Divider(
+            height: 15,
+            thickness: 0,
+          ),
+          itemCount: numFields,
+        ),
+      ),
     );
   }
 
+  Widget formChild(BuildContext context, index) {
+    return Container(
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 233, 228, 228),
+          border: Border.all(
+            width: 2,
+            color: Colors.black,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: dueUntillController,
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                filled: true,
+              ),
+            ),
+            TextFormField(
+              controller: dueUntillController,
+              decoration: const InputDecoration(
+                labelText: 'Value',
+                filled: true,
+              ),
+            ),
+            const IconButton(
+                onPressed: null,
+                icon: Icon(
+                  CupertinoIcons.delete_simple,
+                  color: Colors.redAccent,
+                )),
+          ],
+        ));
+  }
+
+  Widget studentViewPage(BuildContext context) {
+    return const Column(
+      children: [Center(child: const Text("Student View Page"))],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Row(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
+              const Text(
                 'Create Form',
                 style: TextStyle(color: Colors.white),
               ),
-              IconButton(onPressed: null, icon: Icon(Icons.check)),
+              IconButton(
+                onPressed: () => print("Hello World!"),
+                icon: const Icon(
+                  Icons.check,
+                  color: Colors.white,
+                ),
+                style: IconButton.styleFrom(backgroundColor: Colors.green),
+              ),
             ],
           ),
           iconTheme: const IconThemeData(color: Colors.white),
           backgroundColor: const Color(0xff004080),
         ),
-        body: _getTabOptions().elementAt(_currentIndex),
+        body: _widgetTabOptions(context).elementAt(_currentIndex),
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex,
           type: BottomNavigationBarType.fixed,
           items: const [
             BottomNavigationBarItem(
-                icon: Icon(Icons.edit_calendar), 
-                label: 'Edit Form'
-            ),
+                icon: Icon(Icons.calendar_today_rounded), label: 'Add Form'),
             BottomNavigationBarItem(
               icon: Icon(CupertinoIcons.eyeglasses),
               label: 'Student View',
