d4e4cedf614fbe3498a7b4463cacf73693d9d1ab
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2024-07-02 17:56:37 -0400

    Added String Formatting for Dates and necessary dependencies

--- flutter_application/lib/src/forms/create_form.dart
+++ flutter_application/lib/src/forms/create_form.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter/cupertino.dart";
 import "package:http/http.dart" as http;
+import 'package:intl/intl.dart';
 import "dart:convert";
 
 class CreateForm extends StatefulWidget {
@@ -67,6 +68,19 @@ class _CreateFormState extends State<CreateForm> {
     });
   }
 
+  String getDateString(String date) {
+    // Parse the input date string
+    DateTime dateTime = DateTime.parse(date);
+
+    // Define the desired date format
+    DateFormat dateFormat = DateFormat("MM/dd/yy");
+
+    // Format the DateTime object to the desired string format
+    String formattedDate = dateFormat.format(dateTime);
+
+    return formattedDate;
+  }
+
   Future<void> _selectDate(TextEditingController controller) async {
     DateTime? picked = await showDatePicker(
       context: context,
@@ -77,7 +91,7 @@ class _CreateFormState extends State<CreateForm> {
 
     if (picked != null) {
       setState(() {
-        controller.text = picked.toString().split(" ")[0];
+        controller.text = getDateString(picked.toString().split(" ")[0]);
       });
     }
   }
