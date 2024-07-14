e294eaea4efc33550a5b0735568187ad97ab5faa
AishaFathalla <156489851+AishaFathalla@users.noreply.github.com>
2024-07-14 16:29:59 -0400

    fixed packages conflicts

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cron from "node-cron";
 import cors from "cors";
-import cors from "cors";
 
 // Import database client
 import db from "./config.js";
 
-// Import servicess for cron jobss
+// Import services for cron jobs
 import * as ReviewService from './services/reviews.js';
-import * as AnalyticsService from './services/analytics.js';
 import * as AnalyticsService from './services/analytics.js';
 
 // Import routers
@@ -18,7 +16,6 @@ import groupRoutes from './routes/groups.js';
 import workspaceRoutes from './routes/workspaces.js';
 import assignmentRoutes from './routes/assignments.js';
 import reviewRoutes from './routes/reviews.js';
-import analyticsRoutes from './routes/analytics.js';
 import analyticsRoutes from './routes/analytics.js';
 import jwtRoutes from './routes/jwt.js';
 
@@ -31,22 +28,14 @@ const PORT = process.env.PORT || 5000;
 // Initialize the app
 const app = express();
 app.use(express.json());
-//app.use(cors());
 
-// Define CORS options
-const corsOptions = {
-    // TODO update to our domain once deployed
-    //origin: process.env.NODE_ENV === 'production' ? 'https://ourdomainname.com' : 'http://localhost:3000',
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow credentials
-};
-
-// Apply CORS middleware with options
-app.use(cors(corsOptions));
 // Set allowed origins
-const allowedOrigins = ['http://45.55.194.65', 'http://ratemypeer.site', 'http://www.ratemypeer.site'];
-
+const allowedOrigins = [
+    'http://45.55.194.65',
+    'http://ratemypeer.site',
+    'http://www.ratemypeer.site',
+    'http://localhost:3000'
+];
 // Configure CORS options
 const corsOptions = {
     origin: function (origin, callback) {
@@ -74,7 +63,6 @@ app.use("/groups", groupRoutes);
 app.use("/workspaces", workspaceRoutes);
 app.use("/assignments", assignmentRoutes);
 app.use("/reviews", reviewRoutes);
-app.use("/analytics", analyticsRoutes);
 app.use("/analytics", analyticsRoutes);
 app.use("/jwt", jwtRoutes);
 
@@ -112,25 +100,6 @@ cron.schedule('0 * * * * *', async() => {
         ReviewService.createReviews(id);
     });
     console.log(`Started ${ids.length} new assignments`);
-});
-
-// Once per minute cron job
-// Computes analytics for assignments, sets to complete
-cron.schedule('0 * * * * *', async() => {
-    const res = await db.query(
-        `UPDATE assignments
-        SET completed = true
-        WHERE completed = false AND due_date <= $1
-        RETURNING id`,
-        [(new Date(Date.now())).toISOString()]
-    );
-    // Get all ids of review assignments
-    const ids = res.rows.map(obj => obj.id);
-    // Calculate analytics for each assignment
-    ids.forEach(id => {
-        AnalyticsService.calculateAnalytics(id);
-    });
-    console.log(`Calculated analytics for ${ids.length} completed assignments`);
 });
 
 // Once per minute cron job
