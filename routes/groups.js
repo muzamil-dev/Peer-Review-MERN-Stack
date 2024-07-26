d5545a857d3ee776198c18b7ce5fc0e7a217ce3b
ckrick04 <ckr20004@gmail.com>
2024-07-25 20:41:49 -0400

    Started adding new routes, csv import appears to work from limited tests

---
+++ routes/groups.js
@@ -0,0 +1,96 @@
+import express from 'express';
+import pool from '../config.js';
+
+import HttpError from '../services/utils/httpError.js';
+
+import * as GroupService from "../services/groups.js";
+import * as WorkspaceService from "../services/workspaces.js";
+
+const router = express.Router();
+
+// Get a group by its id
+router.get("/:groupId", async(req, res) => {
+    let db, group;
+    const { members } = req.query;
+    const { groupId } = req.params;
+    try{
+        db = await pool.connect();
+        if (members === 'true')
+            group = await GroupService.getByIdWithMembers(db, groupId);
+        else
+            group = await GroupService.getById(db, groupId);
+        return res.json(group);
+    }
+    catch(err){
+        return res.status(err.status || 500).json(
+            { message: err.message }
+        );
+    }
+    finally{
+        if (db) db.release();
+    }
+});
+
+// Create a new group
+router.post("/create", async(req, res) => {
+    let db; // Save the client for use in all blocks
+    try{
+        db = await pool.connect();
+        await db.query('BEGIN');
+        const { userId, workspaceId, name } = req.body;
+        // Check that the required fields are provided
+        if (!workspaceId || !name)
+            throw new HttpError("One or more required fields is missing", 400);
+        // Check that the person creating the group is an instructor
+        await WorkspaceService.checkInstructor(db, userId, workspaceId);
+        // Create the group
+        const group = await GroupService.createGroup(db, workspaceId, name);
+        // Commit and release connection
+        await db.query('COMMIT');
+        // Send data and release
+        return res.status(201).json(group);
+    }
+    catch(err){
+        if (db) 
+            await db.query('ROLLBACK');
+        return res.status(err.status || 500).json(
+            { message: err.message }
+        );
+    }
+    finally{
+        if (db) db.release();
+    }
+});
+
+// Delete a provided group
+router.delete("/:groupId/delete", async(req, res) => {
+    let db; // Save the client for use in all blocks
+    try{
+        db = await pool.connect();
+        await db.query('BEGIN');
+        const { userId } = req.body;
+        const { groupId } = req.params;
+        // Get the workspaceId of the group
+        const workspaceId = (await GroupService.getById(db, groupId)).workspaceId;
+        // Check that the provided user is an instructor of the workspace
+        await WorkspaceService.checkInstructor(db, userId, workspaceId);
+        // Delete the workspace
+        const msg = await GroupService.deleteGroup(db, groupId);
+        // Commit and release connection
+        await db.query('COMMIT');
+        // Send data and release
+        return res.json(msg);
+    }
+    catch(err){
+        if (db) 
+            await db.query('ROLLBACK');
+        return res.status(err.status || 500).json(
+            { message: err.message }
+        );
+    }
+    finally{
+        if (db) db.release();
+    }
+})
+
+export default router;
--- routes/workspaces.js
+++ routes/workspaces.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import multer from 'multer';
+import csv from 'csvtojson';
+
 import pool from '../config.js';
 
 import HttpError from '../services/utils/httpError.js';
@@ -6,8 +9,10 @@ import HttpError from '../services/utils/httpError.js';
 import * as UserService from "../services/users.js";
 import * as WorkspaceService from "../services/workspaces.js";
 import * as GroupService from "../services/groups.js"
+import { convertEmailAndGroupNames } from '../services/utils/conversions.js';
 
 const router = express.Router();
+const upload = multer();
 
 // Get basic information about a workspace
 router.get("/:workspaceId", async(req, res) => {
@@ -79,6 +84,49 @@ router.post("/create", async(req, res) => {
     }
 });
 
+// Import csv woooooo
+router.post("/import", upload.single('csvFile'), async(req, res) => {
+    let db;
+    try{
+        db = await pool.connect();
+        await db.query('BEGIN');
+        const { userId, workspaceId } = req.body;
+
+        // Check that the user is an instructor
+        await WorkspaceService.checkInstructor(db, userId, workspaceId);
+
+        // User is an instructor, get csv data
+        const csvFile = req.file;
+        if (!csvFile) 
+            throw new HttpError('No csv file was provided', 400);
+        // Convert csv data to a string
+        const csvData = csvFile.buffer.toString('utf-8');
+        // Convert csv string to json
+        const jsonData = await csv().fromString(csvData);
+        
+        // Create accounts for anyone without one
+        await UserService.createUsers(db, jsonData);
+        // Create all groups mentioned in the csv
+        const groups = jsonData.map(user => user.groupName);
+        await GroupService.createGroups(db, workspaceId, groups);
+        // Insert all users into their groups
+        const usersAndGroups = await convertEmailAndGroupNames(db, workspaceId, jsonData);
+        await WorkspaceService.insertUsers(db, workspaceId, usersAndGroups);
+        await db.query('COMMIT');
+        return res.status(201).json({ message: "CSV imported successfully"});
+    }
+    catch(err){
+        if (db) 
+            await db.query('ROLLBACK');
+        return res.status(err.status || 500).json(
+            { message: err.message }
+        );
+    }
+    finally{
+        if (db) db.release();
+    }
+});
+
 // Edit a provided workspace
 router.put("/edit", async(req, res) => {
     let db; // Save the client for use in all blocks
@@ -94,6 +142,63 @@ router.put("/edit", async(req, res) => {
         await WorkspaceService.checkInstructor(db, userId, workspaceId);
         // Edit the workspace
         const msg = await WorkspaceService.edit(db, workspaceId, updates);
+        // Commit and release connection
+        await db.query('COMMIT');
+        // Send data and release
+        return res.json(msg);
+    }
+    catch(err){
+        if (db) 
+            await db.query('ROLLBACK');
+        return res.status(err.status || 500).json(
+            { message: err.message }
+        );
+    }
+    finally{
+        if (db) db.release();
+    }
+});
+
+// Remove a user from the workspace
+router.put("/removeUser", async(req, res) => {
+    let db; // Save the client for use in all blocks
+    try{
+        db = await pool.connect();
+        await db.query('BEGIN');
+        const { userId, workspaceId, targetId } = req.body;
+        // Check that the provided user is an instructor of the workspace
+        await WorkspaceService.checkInstructor(db, userId, workspaceId);
+        // Delete the workspace
+        const msg = await WorkspaceService.removeUser(db, targetId, workspaceId);
+        // Commit and release connection
+        await db.query('COMMIT');
+        // Send data and release
+        return res.json(msg);
+    }
+    catch(err){
+        if (db) 
+            await db.query('ROLLBACK');
+        return res.status(err.status || 500).json(
+            { message: err.message }
+        );
+    }
+    finally{
+        if (db) db.release();
+    }
+})
+
+// Delete a workspace, used by instructors
+router.delete("/:workspaceId/delete", async(req, res) => {
+    let db; // Save the client for use in all blocks
+    try{
+        db = await pool.connect();
+        await db.query('BEGIN');
+        const { userId } = req.body;
+        const { workspaceId } = req.params;
+        // Check that the provided user is an instructor of the workspace
+        await WorkspaceService.checkInstructor(db, userId, workspaceId);
+        // Delete the workspace
+        const msg = await WorkspaceService.deleteWorkspace(db, workspaceId);
         // Commit and release connection
         await db.query('COMMIT');
         // Send data and release
