f5b6e99afd4b2a03daf37f4964e2366f8eca73c7
HashDBrown <halkhateeb704@gmail.com>
2024-08-07 17:34:01 -0400

    removed signup from login page

--- web_frontend/src/login_signup/LoginPage.js
+++ web_frontend/src/login_signup/LoginPage.js
@@ -8,7 +8,6 @@ import './LoginPage.css'; // Assuming you save the CSS styles in LoginPage.css
 const LoginPage = () => {
     const [isLoginActive, setIsLoginActive] = useState(true);
     const [loginData, setLoginData] = useState({ email: '', password: '' });
-    const [signupData, setSignupData] = useState({ firstName: '', middleName: '', lastName: '', email: '', password: '', confirmPassword: '' });
     const [signupPasswordRequirements, setSignupPasswordRequirements] = useState({ uppercase: false, specialChar: false, number: false, lowercase: false, length: false });
     const [resetPasswordRequirements, setResetPasswordRequirements] = useState({ uppercase: false, specialChar: false, number: false, lowercase: false, length: false });
     const [showSignupPasswordRequirements, setShowSignupPasswordRequirements] = useState(false);
@@ -29,20 +28,7 @@ const LoginPage = () => {
         setLoginData({ ...loginData, [e.target.name]: e.target.value });
     };
 
-    const handleSignupChange = (e) => {
-
-        const { name, value } = e.target;
-        setSignupData({ ...signupData, [name]: value });
-
-        if (name === 'password') {
-            checkPasswordComplexity(value, 'signup');
-            setShowSignupPasswordRequirements(value.length > 0);
-        }
-
-        if (name === 'confirmPassword') {
-            setShowSignupPasswordsMatch(true);
-        }
-    };
+    
 
     const handleVerificationChange = (e) => {
         setVerificationToken(e.target.value);
@@ -107,42 +93,6 @@ const LoginPage = () => {
         }
     };
     
-    const handleSignup = async (e) => {
-        e.preventDefault();
-        //console.log('Signup button clicked'); // Add this line for debugging
-        if (signupData.password !== signupData.confirmPassword) {
-            enqueueSnackbar('Passwords do not match', { variant: 'error' });
-            return;
-        }
-
-        if (!isPasswordValid('signup')) {
-            enqueueSnackbar('Password does not meet complexity requirements', { variant: 'error' });
-            return;
-        }
-    
-        //console.log('Signup Data:', signupData); // Log the signup data for debugging
-    
-        try {
-            const response = await Api.Users.CreateAccount(
-                signupData.firstName,
-                signupData.lastName,
-                signupData.email,
-                signupData.password
-            );
-            //console.log('Response from API:', response); // Add this line for debugging
-            if (response.status === 201) {
-                enqueueSnackbar('Verification token sent to email', { variant: 'success' });
-                setTempEmail(signupData.email);
-                setIsVerificationActive(true);
-            } else {
-                enqueueSnackbar(`${response.message}`, { variant: 'error' });
-            }
-        } catch (error) {
-            console.error('Error signing up:', error);
-            enqueueSnackbar(`Error signing up: ${error.message}`, { variant: 'error' });
-        }
-    };
-    
     const handleVerify = async (e) => {
         e.preventDefault();
         try {
@@ -153,7 +103,6 @@ const LoginPage = () => {
                 enqueueSnackbar('Verification successful. You can now log in.', { variant: 'success' });
                 setIsLoginActive(true);
                 setIsVerificationActive(false);
-                setSignupData({ firstName: '', middleName: '', lastName: '', email: '', password: '', confirmPassword: '' });
                 setVerificationToken('');
                 //console.log('Verification successful, switched to login form.');
             } else {
@@ -222,33 +171,8 @@ const LoginPage = () => {
                 </div>
             </div>
             <div className="form-container">
-                <div className="slide-controls">
-                    <input
-                        type="radio"
-                        name="slide"
-                        id="login"
-                        checked={isLoginActive}
-                        onChange={() => setIsLoginActive(true)}
-                    />
-                    <input
-                        type="radio"
-                        name="slide"
-                        id="signup"
-                        checked={!isLoginActive}
-                        onChange={() => setIsLoginActive(false)}
-                    />
-                    <label htmlFor="login" className="slide login" onClick={() => setIsLoginActive(true)}>
-                        Login
-                    </label>
-                    <label htmlFor="signup" className="slide signup" onClick={() => setIsLoginActive(false)}>
-                        Signup
-                    </label>
-                    <div className="slider-tab" style={{ left: isLoginActive ? '0%' : '50%' }}></div>
-                </div>
                 <div className="form-inner" style={{ marginLeft: isLoginActive ? '0%' : '-100%' }}>
-                
-            
-                
+
                 {isRequestResetPasswordActive ? (
 
                     <form onSubmit={handleResetPasswordRequest} className="request reset password ">
@@ -267,9 +191,7 @@ const LoginPage = () => {
                             <div className="btn-layer"></div>
                             <input type="submit" value="Send email" />
                         </div>
-                        <div className="signup-link">
-                            <a href="#" onClick={() => setIsRequestResetPasswordActive(false)}> Back to sign in</a>
-                        </div>
+
                         </form>
                         
                     
@@ -353,106 +275,10 @@ const LoginPage = () => {
                             <div className="btn-layer"></div>
                             <input type="submit" value="Login" />
                         </div>
-                        <div className="signup-link">
-                            Not a member? <a href="#" onClick={() => setIsLoginActive(false)}>Signup now</a>
-                        </div>
                         </form>
                     )
                 )}
 
-                {isVerificationActive ? ( /**/
-                <form onSubmit={handleVerify} className="verify">
-                            <div className="field">
-                                <input
-                                    type="text"
-                                    name="token"
-                                    placeholder="Verification Token"
-                                    value={verificationToken}
-                                    onChange={handleVerificationChange}
-                                    required
-                                />
-                            </div>
-                            <div className="field btn">
-                                <div className="btn-layer"></div>
-                                <input type="submit" value="Verify" />
-                            </div>
-                            <div className="signup-link">
-                            Incorrect email? <a href="#" onClick={() => setIsVerificationActive(false)}>Back to sign up</a>
-                        </div>
-                        </form>
-                ):(
-                    
-                    <form onSubmit={handleSignup} className="signup">
-                        <div className="field-row">
-                            <div className="field">
-                                <input
-                                    type="text"
-                                    name="firstName"
-                                    placeholder="First Name"
-                                    value={signupData.firstName}
-                                    onChange={handleSignupChange}
-                                    //required
-                                />
-                            </div>
-                            <div className="field">
-                                <input
-                                    type="text"
-                                    name="lastName"
-                                    placeholder="Last Name"
-                                    value={signupData.lastName}
-                                    onChange={handleSignupChange}
-                                    //required
-                                />
-                            </div>
-                        </div>
-                        <div className="field">
-                            <input
-                                type="text"
-                                name="email"
-                                placeholder="Email Address"
-                                value={signupData.email}
-                                onChange={handleSignupChange}
-                                required
-                            />
-                        </div>
-                        <div className="field">
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Password"
-                                value={signupData.password}
-                                onChange={handleSignupChange}
-                                required
-                            />
-                        </div>
-                        {showSignupPasswordRequirements && (
-                                <div className="password-requirements" style={{ paddingTop: '7px' }}>
-                                    {!signupPasswordRequirements.uppercase && <p style={{ color: '#004080' }}>At least one uppercase letter is required</p>}
-                                    {signupPasswordRequirements.uppercase && !signupPasswordRequirements.specialChar && <p style={{ color: '#004080' }}>At least one special character is required</p>}
-                                    {signupPasswordRequirements.uppercase && signupPasswordRequirements.specialChar && !signupPasswordRequirements.number && <p style={{ color: '#004080' }}>At least one number is required</p>}
-                                    {signupPasswordRequirements.uppercase && signupPasswordRequirements.specialChar && signupPasswordRequirements.number && !signupPasswordRequirements.lowercase && <p style={{ color: '#004080' }}>At least one lowercase letter is required</p>}
-                                    {signupPasswordRequirements.uppercase && signupPasswordRequirements.specialChar && signupPasswordRequirements.number && signupPasswordRequirements.lowercase && !signupPasswordRequirements.length && <p style={{ color: '#004080' }}>At least 8 characters are required</p>}
-                                </div>
-                        )}
-                        <div className="field">
-                            <input
-                                type="password"
-                                name="confirmPassword"
-                                placeholder="Confirm password"
-                                value={signupData.confirmPassword}
-                                onChange={handleSignupChange}
-                                required
-                            />
-                        </div>
-                        {showSignupPasswordsMatch && signupData.confirmPassword && signupData.password !== signupData.confirmPassword && (
-                                <p style={{ color: '#004080', paddingTop: '7px' }}>Passwords do not match</p>
-                            )}
-                        <div className="field btn">
-                            <div className="btn-layer"></div>
-                            <input type="submit" value="Signup" />
-                        </div>
-                    </form>
-                )}
                 </div>
             </div>
             {/*{isLoading && <div className="loading">Processing...</div>} {/* Loading indicator */}
