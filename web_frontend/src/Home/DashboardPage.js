481d77b1d32b6eb8802681fedcf694e9795bc497
HashDBrown <halkhateeb704@gmail.com>
2024-08-08 09:31:19 -0400

    Removed join workspace in dashboard page. Also, got rid of no longer used fields when creating workspace

--- web_frontend/src/Home/DashboardPage.js
+++ web_frontend/src/Home/DashboardPage.js
@@ -9,10 +9,6 @@ import { enqueueSnackbar } from 'notistack';
 const DashboardPage = () => {
     const [workspaces, setWorkspaces] = useState([]);
     const [newWorkspaceName, setNewWorkspaceName] = useState('');
-    const [newWorkspaceDomains, setNewWorkspaceDomains] = useState('');
-    const [inviteCode, setInviteCode] = useState('');
-    const [maxGroupSize, setMaxGroupSize] = useState('');
-    const [numGroups, setNumGroups] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -50,43 +46,6 @@ const DashboardPage = () => {
         fetchWorkspaces();
     }, [navigate]);
 
-    const handleJoinWorkspace = async () => {
-        const token = localStorage.getItem('accessToken');
-        if (!token) {
-            navigate('/');
-            return;
-        }
-
-        const decodedToken = jwtDecode(token);
-        const userId = decodedToken.userId;
-
-        try {
-            const response = await Api.Workspaces.JoinWorkspace(userId, inviteCode);
-            if (response.success) {
-                const fetchWorkspaces = async () => {
-                    try {
-                        const response = await Api.Users.getUserWorkspaces(token);
-                        if (response.status === 200) {
-                            setWorkspaces(response.data);
-                        } else {
-                            console.error('Failed to fetch workspaces:', response.message);
-                        }
-                    } catch (error) {
-                        console.error('Error fetching workspaces:', error);
-                    }
-                };
-                enqueueSnackbar('Successfully joined workspace.', { variant: 'success' });
-                fetchWorkspaces();
-            } else {
-                enqueueSnackbar('Failed to join workspace.', { variant: 'error' });
-                //console.error('Failed to join workspace:', response.message);
-            }
-        } catch (error) {
-            enqueueSnackbar('Error joining workspace.', { variant: 'error' });
-            //console.error('Error joining workspace:', error);
-        }
-    };
-
     const handleAddWorkspace = async () => {
         const token = localStorage.getItem('accessToken');
         if (!token) {
@@ -110,31 +69,7 @@ const DashboardPage = () => {
         // Numbers and special characters are not allowed
         // Allowed domains should be separated by commas
         // Should be a "." in each domain and characters before and after the "."
-        const domainsArray = newWorkspaceDomains.split(',').map(domain => domain.trim()).filter(domain => domain);
     
-        const isValidDomain = (domain) => {
-            const domainRegex = /^[a-zA-Z]+\.[a-zA-Z]+$/;
-            return domainRegex.test(domain);
-        };
-    
-        for (const domain of domainsArray) {
-            if (!isValidDomain(domain)) {
-                //console.error(`Invalid domain: ${domain}`);
-                enqueueSnackbar(`Invalid domain: ${domain}`, { variant: 'error' });
-                return;
-            }
-        }
-    
-        // Validate that numGroups and maxGroupSize are numbers
-        if (numGroups && isNaN(parseInt(numGroups, 10))) {
-            enqueueSnackbar('Number of groups must be a valid number.', { variant: 'error' });
-            return;
-        }
-    
-        if (maxGroupSize && isNaN(parseInt(maxGroupSize, 10))) {
-            enqueueSnackbar('Max group size must be a valid number.', { variant: 'error' });
-            return;
-        }
     
         try {
             //check if name is empty
@@ -145,9 +80,6 @@ const DashboardPage = () => {
             const response = await Api.Workspaces.CreateWorkspace(
                 newWorkspaceName,
                 userId,
-                domainsArray,
-                maxGroupSize ? parseInt(maxGroupSize, 10) : undefined,
-                numGroups ? parseInt(numGroups, 10) : undefined
             );
     
             if (response.status === 201 || response.status === 200) {
@@ -168,15 +100,9 @@ const DashboardPage = () => {
                     workspaceId: response.workspaceId,
                     name: newWorkspaceName,
                     role: 'Admin',
-                    maxGroupSize: maxGroupSize,
-                    numGroups: numGroups
                 };
                 setWorkspaces([...workspaces, newWorkspace]);
                 setNewWorkspaceName('');
-                setNewWorkspaceDomains('');
-                setInviteCode('');
-                setMaxGroupSize('');
-                setNumGroups('');
                 enqueueSnackbar('Workspace created successfully.', { variant: 'success' });
             } else {
                 //console.error('Failed to create workspace:', response.message);
@@ -239,39 +165,9 @@ const DashboardPage = () => {
                     ))}
                 </div>
                 <div className="workspace-actions text-center">
-                    <button type="button" className="btn btn-primary btn-large mb-4 btn-center" data-toggle="modal" data-target="#joinWorkspaceModal">
-                        Join
-                    </button>
                     <button type="button" className="btn btn-success btn-large mb-4 btn-center" data-toggle="modal" data-target="#createWorkspaceModal">
                         Create
                     </button>
-                </div>
-            </div>
-
-            {/* Join Workspace Modal */}
-            <div className="modal fade" id="joinWorkspaceModal" tabIndex="-1" aria-labelledby="joinWorkspaceModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="joinWorkspaceModalLabel">Join Workspace</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <input
-                                type="text"
-                                placeholder="Enter code"
-                                value={inviteCode}
-                                onChange={(e) => setInviteCode(e.target.value)}
-                                className="form-control mb-2"
-                            />
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary" onClick={handleJoinWorkspace}>Join</button>
-                        </div>
-                    </div>
                 </div>
             </div>
 
@@ -292,27 +188,6 @@ const DashboardPage = () => {
                                 placeholder="Workspace Name"
                                 value={newWorkspaceName}
                                 onChange={(e) => setNewWorkspaceName(e.target.value)}
-                                className="form-control mb-2"
-                            />
-                            <input
-                                type="text"
-                                placeholder="Domain Restrictions (comma separated)"
-                                value={newWorkspaceDomains}
-                                onChange={(e) => setNewWorkspaceDomains(e.target.value)}
-                                className="form-control mb-2"
-                            />
-                            <input
-                                type="text"
-                                placeholder="Max Group Size (optional)"
-                                value={maxGroupSize}
-                                onChange={(e) => setMaxGroupSize(e.target.value)}
-                                className="form-control mb-2"
-                            />
-                            <input
-                                type="text"
-                                placeholder="Number of Groups (optional)"
-                                value={numGroups}
-                                onChange={(e) => setNumGroups(e.target.value)}
                                 className="form-control mb-2"
                             />
                         </div>
