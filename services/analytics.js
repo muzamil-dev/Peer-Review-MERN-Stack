77ab00834ba200e311a8c6ad472180b8a1d4683d
ckrick04 <ckr20004@gmail.com>
2024-08-09 13:59:47 -0400

    Added a function to get all users who haven't completed all of their reviews, as well as imported the function to get average ratings by assignment

--- services/analytics.js
+++ services/analytics.js
@@ -2,8 +2,68 @@ import HttpError from "./utils/httpError.js";
 
 import * as AssignmentService from "../services/assignments.js";
 
+export const getByAssignment = async(db, assignmentId, page, perPage) => {
+    // Set start point
+    const startIndex = perPage * (page - 1);
+    const res = await db.query(
+        `SELECT a.user_id AS "userId",
+        u.first_name AS "firstName",
+        u.last_name AS "lastName",
+        round(a.average_rating, 2) AS "averageRating"
+        FROM analytics AS a
+        JOIN users AS u
+        ON u.id = a.user_id
+        WHERE a.assignment_id = $1 AND a.average_rating IS NOT NULL
+        ORDER BY average_rating
+        LIMIT $2 OFFSET $3`,
+        [assignmentId, perPage, startIndex]
+    );
+    return res.rows.map(row => ({
+        ...row,
+        averageRating: parseFloat(row.averageRating)
+    }));
+}
+
+// Function to get users who haven't completed all of their reviews
+export const getIncomplete = async(db, assignmentId, page, perPage) => {
+    // Set start point
+    const startIndex = perPage * (page - 1);
+    const res = await db.query(
+        `WITH all_results AS
+        (SELECT u.id AS "userId",
+        u.first_name AS "firstName",
+        u.last_name AS "lastName",
+        count(CASE WHEN r.completed = true THEN 1 END)::int AS "completedReviews",
+        count(r.id)::int AS "totalReviews"
+        FROM assignments AS a
+        JOIN reviews AS r
+        ON r.assignment_id = a.id
+        JOIN users AS u
+        ON u.id = r.user_id
+        WHERE a.id = $1
+        GROUP BY u.id),
+        
+        filtered_ordered_results AS 
+        (SELECT *
+        FROM all_results
+        WHERE "completedReviews" != "totalReviews"
+        ORDER BY "completedReviews"::float / "totalReviews",
+        "lastName", "firstName"),
+
+        limited_results AS
+        (SELECT * FROM filtered_ordered_results LIMIT $2 OFFSET $3)
+        
+        SELECT
+            (SELECT count(*)::int FROM filtered_ordered_results) 
+            AS "totalResults",
+            (SELECT jsonb_agg(limited_results)
+            FROM limited_results) AS "results"`,
+        [assignmentId, perPage, startIndex]
+    );
+    return res.rows;
+}
+
 // Update analytics for a specific user. Used when submitting reviews
-// Must be used within a transaction
 export const updateAnalytics = async(db, userId, assignmentId) => {
     // Compute analytics and insert
     const res = await db.query(
